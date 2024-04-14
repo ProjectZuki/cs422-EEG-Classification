ca9673e11ab7ff71395932b18971304699cd4516
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2024-04-14 04:41:19 -0700

    Implemented data preprocessing, added starter unit test file
    Note: unittest is in progress, preprocessing uses KNN to impute missing variables, if any. Work in progress.

--- train.py
+++ train.py
@@ -6,9 +6,7 @@ import pandas as pd
 import numpy as np
 import sys
 import os
-
-# user defined modules
-import data_load as dl
+import logging
 
 # import tensorflow as tf
 # import tensorflow.keras.layers as layers
@@ -16,11 +14,58 @@ import data_load as dl
 # import tensorflow.keras.optimizers as optimizers
 # import tensorflow.keras.backend as K
 
-from sklearn.model_selection import KFold
+from sklearn.impute import KNNImputer
+
+# user defined modules
+import data_load as dl
+
+def impute_null(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Impute missing values using KNN to determine the value of the missing data.
+
+    Args:
+        data (pd.DataFrame): The DataFrame to impute.
+
+    Returns:
+        pd.DataFrame: The DataFrame with imputed values.
+    """
+
+    if data.empty:
+        return data
+
+    # manually define column names
+    columns = data.columns
+
+    # exclude non-numeric columns (i.e. expert_concensus column)
+    numeric_columns = data.select_dtypes(include=[np.number]).columns
+    data_numeric = data[numeric_columns]
+
+    # use KNN to impute values
+    impute = KNNImputer(n_neighbors=5)
+    imputed_values = impute.fit_transform(data_numeric)
+
+    df = pd.DataFrame(imputed_values, columns=numeric_columns, index=data_numeric.index)
+
+    # combine non-numeric columns with imputed values
+    for col in columns:
+        if col not in numeric_columns:
+            df[col] = data[col]
+
+    return df
+
+
+
+def preprocess_data(train_data: pd.DataFrame, test_data: pd.DataFrame) -> tuple:
+    # handle missing values
+    train_data = impute_null(train_data)
+    test_data = impute_null(test_data)
+
+    return train_data, test_data
 
 def main():
 
     if len(sys.argv) < 3:
+        print("Error: Expected 3 arguments, recieved", len(sys.argv))
         print("Usage: python", sys.argv[0], "<train_file> <test_file>")
         print("Exiting ...")
         exit(1)
@@ -30,12 +75,18 @@ def main():
     train_data, test_data = dl.read_data(sys.argv[1], sys.argv[2])
 
     # Exploratory Data Analysis
-    print(train_data.head())
-    print("-" * 50)
-    print(train_data.info())
-    print("-" * 50)
-    print(train_data.describe())
-    print("-" * 50)
+    # print(train_data.head())
+    # print("-" * 50)
+    # print(train_data.info())
+    # print("-" * 50)
+    # print(train_data.describe())
+    # print("-" * 50)
+
+    # =========================== Data Preprocessing ===========================
+    train_data, test_data = preprocess_data(train_data, test_data)
+    # ============================ Categorical Data ============================
+
+    # feature scaling
 
 if __name__ == "__main__":
     main()
@@ -45,7 +96,7 @@ if __name__ == "__main__":
     TODO
     
     Data Loading: x
-    Exploratory Data Analysis (EDA): 
+    Exploratory Data Analysis (EDA): x
     Data Preprocessing:
     Feature Engineering:
     Model Training:
