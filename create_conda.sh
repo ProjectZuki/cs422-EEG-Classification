1e1c3a9c8085f34a8fce3556518ba3ab3b19c475
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-05-01 02:04:24 +0000

    Updated missing dep

--- create_conda.sh
+++ create_conda.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Create a new conda environment
-conda create -n EEG tensorflow numpy matplotlib pandas scikit-learn tqdm keras
+conda create -n EEG tensorflow numpy matplotlib pandas scikit-learn tqdm keras joblib
